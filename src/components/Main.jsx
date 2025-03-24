f11cd2d48722736f816234097ae214e5670fb170
“alerenna” <alessandrorennafm@gmail.com>
2025-03-24 13:10:46 +0800

    Edit Card component styles and add “LOAD MORE” button in Main section; improve structure and layout of buy section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,29 +10,37 @@ export default function Main() {
             <div className="container">
                 <div className="cardSection">
                     <CardList />
+                    <button className='btn'>
+                        LOAD MORE
+                    </button>
                 </div>
 
+
                 <div className="buySection">
-                    <div className="icon">
-                        <img src="/images/buy-comics-digital-comics.png" alt="" />
-                        <span id="iconName">DIGITAL COMICS</span>
+                    <div className="container">
+                        <div className="icon">
+                            <img src="/images/buy-comics-digital-comics.png" alt="" />
+                            <span id="iconName">DIGITAL COMICS</span>
+                        </div>
+                        <div className="icon">
+                            <img src="/images/buy-comics-merchandise.png" alt="" />
+                            <span id="iconName">DC MERCHANDISE</span>
+                        </div>
+                        <div className="icon">
+                            <img src="/images/buy-comics-subscriptions.png" alt="" />
+                            <span id="iconName">SUBSCRIPTION</span>
+                        </div>
+                        <div className="icon">
+                            <img src="/images/buy-comics-shop-locator.png" alt="" />
+                            <span id="iconName">COMIC SHOP LOCATOR</span>
+                        </div>
+                        <div className="icon">
+                            <img src="/images/buy-dc-power-visa.svg" alt="" />
+                            <span id="iconName">DC POWER VISA</span>
+                        </div>
                     </div>
-                    <div className="icon">
-                        <img src="/images/buy-comics-merchandise.png" alt="" />
-                        <span id="iconName">DC MERCHANDISE</span>
-                    </div>
-                    <div className="icon">
-                        <img src="/images/buy-comics-subscriptions.png" alt="" />
-                        <span id="iconName">SUBSCRIPTION</span>
-                    </div>
-                    <div className="icon">
-                        <img src="/images/buy-comics-shop-locator.png" alt="" />
-                        <span id="iconName">COMIC SHOP LOCATOR</span>
-                    </div>
-                    <div className="icon">
-                        <img src="/images/buy-dc-power-visa.svg" alt="" />
-                        <span id="iconName">DC POWER VISA</span>
-                    </div>
+
+
                 </div>
             </div>
         </main>
